b4414156a7d61a52472b703d5590d00ebe3b3337
Aleksandr Charushin <charushinas@gmail.com>
2023-06-29 12:08:40 +0300

    Make headhunter_api.py, superjob_api.py

--- api/headhunter_api.py
+++ api/headhunter_api.py
@@ -1,8 +1,6 @@
 from api.abstract_api import AbstractAPI
-
-import os
+from vacancy.vacancy import Vacancy
 import requests
-import ujson
 
 
 class HeadHunter(AbstractAPI):
@@ -10,30 +8,65 @@ class HeadHunter(AbstractAPI):
     def __init__(self, keyword: str) -> None:
         self.keyword = keyword
         self.__params = {
-            'text': 'NAME' + self.keyword,  # Поиск вакансии по передаваемой строке
+            'text': 'NAME:' + self.keyword,  # Поиск вакансии по передаваемой строке
             'area': 113,  # Поиск осуществляется по вакансиям города Москва
             'page': 0,  # Индекс страницы с вакансиями на ХХ
             'per_page': 100  # Количество вакансий на 1 странице
         }
 
-    def get_vacancies(self) -> dict:
+    def __parse(self, vocations: list):
+        """Обрабатывает вакансии"""
+        data = []
+        for v in vocations:
+            try:
+                salary_from = v["salary"]["salary_from"]
+            except (KeyError, TypeError):
+                salary_from = 0
+            try:
+                salary_to = v["salary"]["salary_to"]
+            except (KeyError, TypeError):
+                salary_to = 0
+            try:
+                salary_currency = v["salary"]["currency"]
+            except (KeyError, TypeError):
+                salary_currency = "USD"
+            try:
+                salary_gross = v["salary"]["gross"]
+            except (KeyError, TypeError):
+                salary_gross = False
+
+            data.append(Vacancy(
+                **{
+                    "platform": "HeadHunter",
+                    "city": v["area"]["name"],
+                    "vacancy": v["name"],
+                    "link": v["alternate_url"],
+                    "experience": v["experience"]["name"],
+                    "salary": {"from": salary_from,
+                               "to": salary_to,
+                               "currency": salary_currency,
+                               "gross": salary_gross},
+                    "description": v["snippet"]["responsibility"]
+                }
+            ))
+        return data
+
+    def get_vacancies(self) -> list[Vacancy]:
         """
         Метод для получения данных по заданной вакансии
-        :return: список вакансий в формате json
+        :return: список экземпляров класса Vacancy
         """
-        hh_req = requests.get('https://api.hh.ru/vacancies', params=self.__params)
-        hh_row_data = hh_req.content.decode()
-        hh_req.close()
-        hh_json = ujson.loads(hh_row_data)
+        vocation_data = []
+        for i in range(1):
+            self.__params['page'] = i
+            response = requests.get('https://api.hh.ru/vacancies', params=self.__params)
+            row_data = response.json()['items']  # json method requests
+            vocation_data.extend(row_data)
 
-        return hh_json
+        return self.__parse(vocation_data)
 
     def __str__(self):
         return self.keyword
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.keyword}')"
-
-
-hh = HeadHunter('Python')
-print(hh.get_vacancies())
--- api/superjob_api.py
+++ api/superjob_api.py
@@ -1,45 +1,79 @@
 from api.abstract_api import AbstractAPI
-
-import os
+from vacancy.vacancy import Vacancy
 import requests
-import ujson
+import os
 
 
 class SuperJob(AbstractAPI):
 
-    # api: str = os.environ.get("SUPERJOB_API")
+    api: str = os.environ.get("SUPERJOB_API")
 
     def __init__(self, keyword: str) -> None:
         self.keyword = keyword
         self.__params = {
-            'keywords': [keyword],  # Ключевое слово в вакансии
+            'keywords': self.keyword,  # Ключевое слово в вакансии
             'payment_from': 0,  # Минимальна зарплата
-            'published': 1
+            'published': 1,
+            'page': 0,
+            'count': 100
         }
 
-    def get_vacancies(self):
+    def __parse(self, vocations: list):
+        data = []
+        for v in vocations:
+            try:
+                salary_from = v["payment_from"]
+            except (KeyError, TypeError):
+                salary_from = 0
+            try:
+                salary_to = v["payment_to"]
+            except (KeyError, TypeError):
+                salary_to = 0
+            try:
+                salary_currency = v["salary"]["currency"]
+            except (KeyError, TypeError):
+                salary_currency = "USD"
+            try:
+                salary_gross = v["salary"]["gross"]
+            except (KeyError, TypeError):
+                salary_gross = False
+
+            data.append(Vacancy(
+                **{
+                    "platform": "SuperJob",
+                    "city": v["town"]["title"],
+                    "vacancy": v["profession"],
+                    "link": v["link"],
+                    "experience": v["experience"]["title"],
+                    "salary": {"from": salary_from,
+                               "to": salary_to,
+                               "currency": salary_currency,
+                               "gross": salary_gross},
+                    "description": v["candidat"]
+                }
+            ))
+        return data
+
+    def get_vacancies(self) -> list[Vacancy]:
+        vocation_data = []
         api_url = 'https://api.superjob.ru/2.0/vacancies/'
         headers = {
             'Host': 'api.superjob.ru',
-            'X-Api-App-Id': 'v3.r.137630020.d0ae84775cfab5b574e35b4464d652a3fc2b0ada.f737d25ba5f3a6c85fb84b5e16edb78e6cd9f545',
+            'X-Api-App-Id': self.api,
             'Authorization': 'Bearer r.000000010000001.example.access_token',
             'Content-Type': 'application / x-www-form-urlencoded'
         }
 
-        response = requests.get(api_url, params=self.__params, headers=headers)
-        # sj_json = response.json()
+        for i in range(1):
+            self.__params['page'] = i
+            response = requests.get(api_url, params=self.__params, headers=headers)
+            row_data = response.json()['objects']
+            vocation_data.extend(row_data)
 
-        row_date = response.content.decode()
-        sj_json = ujson.loads(row_date)
-
-        return sj_json
+        return self.__parse(vocation_data)
 
     def __str__(self):
         return self.keyword
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.keyword}')"
-
-
-sj = SuperJob('Python')
-print(sj.get_vacancies())
